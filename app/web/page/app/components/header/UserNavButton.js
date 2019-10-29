d8b0994e5180d2db905a2e708a41e55d19d29b64
aodun <aodun.czp@alipay.com>
2019-10-30 01:45:06 +0800

    done

--- app/web/page/app/components/header/UserNavButton.js
+++ app/web/page/app/components/header/UserNavButton.js
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { Dropdown, Icon } from "antd";
 import UserNavMenuContainer from '../../containers/UserNavMenuContainer'
-
 import styles from "./Header.module.css";
 
 const UserNavButton = () => {
@@ -13,7 +11,8 @@ const UserNavButton = () => {
       trigger={['click']}
       placement='bottomCenter'
     >
-      <Icon type="user" theme="outlined" title='Navigation'/>
+      {/* <Icon type="user" theme="outlined" title='Navigation'/> */}
+      <img className={styles.avatar} src={require('../../../../asset/images/favicon.png')}/> 
     </Dropdown>
   )
 };
