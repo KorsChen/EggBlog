39707d6e68f31a560c9b4710b2bc1314b5ae19eb
aodun <aodun.czp@alipay.com>
2019-10-30 15:44:36 +0800

    webpack thin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
   target: 'web',
   entry: {
@@ -5,7 +8,28 @@ module.exports = {
   },
   plugins:[
     { serviceworker: true },
-    '@babel/plugin-transform-runtime'
+    '@babel/plugin-transform-runtime',
+    new UglifyJsPlugin({
+      parallel: 4,
+      uglifyOptions: {
+        output: {
+          comments: false,
+          beautify: false
+        },
+        warnings: false
+      },
+      cache: true
+    }),
+    new CompressionPlugin({
+      filename: '[path].gz[query]', //目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
+      algorithm: 'gzip',//算法
+      test: new RegExp(
+        '\\.(js|css)$'    //压缩 js 与 css
+      ),
+      threshold: 10240,//只处理比这个值大的资源。按字节计算
+      minRatio: 0.8//只有压缩率比这个值小的资源才会被处理
+    })
+  
   ],
   module: {
     rules: [
