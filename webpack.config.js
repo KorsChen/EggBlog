492dedb4dda90bbbd603097f1ae24bc50b93676e
aodun <aodun.czp@alipay.com>
2019-10-31 18:33:30 +0800

    output

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,6 @@ module.exports = {
   entry: {
     app: 'app/web/page/app/index.js'
   },
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins:[
     { serviceworker: true },
     '@babel/plugin-transform-runtime',
